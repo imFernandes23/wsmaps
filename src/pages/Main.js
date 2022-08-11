c7746a35964e0507fe40b35a720c24a4b6d97050
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2022-08-11 09:48:43 -0300

    add map

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,16 @@
 import React from "react";
+import {useLoadScript} from '@react-google-maps/api'
+import Map from '../components/Map/Map'
+
 function Main(){
-    return(
-        <div>Main em construção</div>
-    )
+    const {isLoaded} = useLoadScript({
+        googleMapsApiKey: 'AIzaSyCGhU6kv2oz6AIW4LbG-eO3AraMqmIsAdw',
+        libraries: ['places','visualization']
+    })
+
+    if(!isLoaded) return( <div className="Page" >Loading...</div>)
+    return <div className="Page">
+        <Map />
+    </div>
 }
 export default Main

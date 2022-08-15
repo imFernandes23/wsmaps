fdb152f96ddf292a6868160eb29025ba698c7ac4
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2022-08-15 14:47:14 -0300

    turn-marker-on

---
+++ src/components/Map/Index.js
@@ -0,0 +1,36 @@
+import React, {useMemo, onLoad} from "react";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import {MapStyle} from "./MapStyle";
+
+export default function Index() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyCGhU6kv2oz6AIW4LbG-eO3AraMqmIsAdw'
+    });
+
+    if (!isLoaded) return <div>Loading..</div>
+    return <Map />;
+}
+
+
+
+function Map(){
+    const center = useMemo(() => ({lat: -1.394782568744898,lng: -48.41606140136719}),  [])
+    const options = useMemo(() => ({
+        disableDefaultUI: true,
+        clickableIcons: false,
+        styles: MapStyle,
+        
+    }), []) 
+
+    return (
+        <GoogleMap 
+            zoom={12.8} 
+            center={center} 
+            mapContainerClassName="map-container"
+            options={options}
+            onLoad={onLoad}
+        >
+            <MarkerF position={center}/>
+        </GoogleMap>
+    )
+}
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,11 @@
 import React from "react";
 import {useLoadScript} from '@react-google-maps/api'
-import Map from '../components/Map/Map'
+// import Map from '../components/Map/Map'
+import Index from "../components/Map/Index"
 
 function Main(){
-    const {isLoaded} = useLoadScript({
-        googleMapsApiKey: 'AIzaSyCGhU6kv2oz6AIW4LbG-eO3AraMqmIsAdw',
-        libraries: ['places','visualization']
-    })
-
-    if(!isLoaded) return( <div className="Page" >Loading...</div>)
     return <div className="Page">
-        <Map />
+        <Index />
     </div>
 }
 export default Main

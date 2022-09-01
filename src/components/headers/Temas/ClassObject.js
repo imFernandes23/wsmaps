3b1ac70a106768aaf929eb8a10dbaf76537a1fd5
imFernandes23 <eduardosage2302@gmail.com>
2022-09-01 16:27:11 -0300

    ..

--- src/components/headers/Temas/ClassObject.js
+++ src/components/headers/Temas/ClassObject.js
@@ -9,25 +9,29 @@ export default function ClassObject(props){
 
     return(<>
     
-        <div className={isOpen ? 'class-object active': 'class-object'} onClick={() => {setIsOpen(!isOpen)
-        if(isOpen === false){props.setRequestClass(props.id)}}}>
-        <p className="name">{props.name}</p>
-        <AiIcons.AiOutlineRight className={isOpen ? 'icon active' : 'icon'}/>
-        </div>
-        {isOpen ? (<div className="childrens">
-            <div className="childrens-list">
-                {props.children ? 
-                (<>{
-                    props.children.map((item, index) => {
-                        return(<SubClassObject
-                            key={`class_${props.id}_sub${index}`}
-                            subname={item.name}
-                            id={item.id}
-                            setRequestSubClass={props.setRequestSubClass}
-                        />)
-                    })
-                }</>): console.log('false')}
+        <div className={isOpen ? 'class-object active': 'class-object'} >
+            <div className="object-content" onClick={() => {setIsOpen(!isOpen)
+            if(isOpen === false){props.setRequestClass(props.id)}}}>
+                <p className="name">{props.name}</p>
+                <AiIcons.AiOutlineRight className={isOpen ? 'icon active' : 'icon'}/>
             </div>
-            </div>):(<></>)}
+
+            
+                <div className="childrens-list">
+                    {props.children ? 
+                    (<>{
+                        props.children.map((item, index) => {
+                            return(<SubClassObject
+                                key={`class_${props.id}_sub${index}`}
+                                subname={item.name}
+                                id={item.id}
+                                setRequestSubClass={props.setRequestSubClass}
+                            />)
+                        })
+                    }</>): console.log('false')}
+                </div>
+
+        </div>
+        
         </>)
 }

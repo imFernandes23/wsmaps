ae0de980d3e7edeb6b142aa7c07c3042d1bf5993
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2022-09-08 15:34:29 -0300

    safe-place3

--- src/components/Map/ThemesDrawer.js
+++ src/components/Map/ThemesDrawer.js
@@ -5,8 +5,23 @@ import { useState, useEffect} from  'react'
 import './ThemesDrawer.css'
 
 function ThemesDrawer(props){
+    const icon = {
+        path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z',
+        scale: 1.3,
+        fillColor: props.element.color,
+        fillOpacity: 1,
+        strokeColor: '#000',
+        strokeWeight: 1,
+        className: 'icons-theme'
+    }
+    
     return(<>
-        <MarkerF position={props.element.coord}/>
+        <MarkerF 
+            position={props.element.coord}
+            icon={icon}
+
+                
+            />
         <OverlayView position={props.element.coord} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
             <div 
             style={{'--MainColor':props.element.color}} 
@@ -16,8 +31,12 @@ function ThemesDrawer(props){
             </div>
             
         </OverlayView>
+
     </>)
 
 }
+
+
+
 
 export default ThemesDrawer

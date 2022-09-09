b73f869b7e4817a584518f136b7434e5d58d058f
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2022-09-09 16:06:13 -0300

    markers+info

--- src/components/Map/ThemesDrawer.js
+++ src/components/Map/ThemesDrawer.js
@@ -5,17 +5,38 @@ import { useState, useEffect} from  'react'
 import './ThemesDrawer.css'
 
 function ThemesDrawer(props){
-    
+    const [isOpen, setIsOpen] = useState(false)
+    let name = props.element.name
+    let subName = props.element.subName
+
+    if(name === ''){
+        name = 'Sem Nome'
+    }
+
+    function handleClick(){
+        setIsOpen(!isOpen)
+
+        console.log('cliquei ' + name + subName)
+    }
+
     return(<>
         <OverlayView position={props.element.coord} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
         <>
             <div 
             style={{'--MainColor':props.element.color}} 
-            className='marker deactive'
+            className={isOpen ? 'marker active' : 'marker'}
+            onClick={() => handleClick()}
             >
                 <img className="img-icon" src={props.element.icon}/>
+
+                {isOpen ? (<>
+
+                        <p className="eleName">{name}</p>
+                        <br/>
+                        <p className="subName">{subName}</p>
+                </>) : (<></>)}
+
             </div>
-            <div style={{'--MainColor':props.element.color}} className="pointer" ></div>
         </>    
         </OverlayView>
 

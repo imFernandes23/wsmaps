180cb796fd4a651531e31261b33761544ccb3e30
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2022-12-21 18:22:17 -0300

    statistic-midia

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from "react";
 import RegionChart from "../components/selectors/RegionChart";
 import {StatisticData} from "../data/StatisticData";
+import ChartsTable from "../components/tables/ChartsTable";
 
 function Statistics(){
     const [controlChart , setControlChart] = useState([true, true, true, true, true, true, true])
 
-
+    useEffect(() => {
+        console.log('mofidy data')
+    },[controlChart])
 
     return(
         <div className="Page">
@@ -14,6 +17,8 @@ function Statistics(){
                 controlChart={controlChart}
                 setControlChart={setControlChart}
             />
+
+            <ChartsTable/>
         </div>
     )
 }
